a46f2a78be7e205c3c2405f10a51e504323fb161
ar <krarvind@gmail.com>
2012-09-29 14:54:41 -0300

    setting vsync to true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,7 +116,7 @@ TARGET_DISABLE_TRIPLE_BUFFERING := false
 BOARD_ALLOW_EGL_HIBERNATION := true
 
 # hwcomposer: custom vsync ioctl
-BOARD_CUSTOM_VSYNC_IOCTL := false
+BOARD_CUSTOM_VSYNC_IOCTL := true
 
 # Suspend in charger to disable capacitive keys
 BOARD_ALLOW_SUSPEND_IN_CHARGER := true
